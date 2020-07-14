cdc1e0c79bbae6e0486300610de59d7789642eee
davidromero <d-romero@live.com>
2020-07-14 00:01:27 -0600

    adwords_automations service

---
+++ adwords_automation/chalicelib/custom_responses.py
@@ -0,0 +1,13 @@
+from chalice import Response
+
+response_headers = {'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type'}
+
+
+def get_base_res():
+    return Response(
+        status_code=200,
+        body={'status': 200, 'payload': 'adwords_automation service running...'},
+        headers=response_headers
+    )
