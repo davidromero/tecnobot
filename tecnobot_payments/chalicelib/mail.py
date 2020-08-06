238bc00946999b207e010e1945845e68a55fc7b8
davidromero <d-romero@live.com>
2020-08-06 00:47:36 -0600

    Refactor mail, only save payment if was found and recipient is correct

--- tecnobot_payments/chalicelib/mail.py
+++ tecnobot_payments/chalicelib/mail.py
@@ -9,11 +9,11 @@ logger.setLevel(logging.INFO)
 
 
 def get_messages(transaction_number):
-    mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    mail = imaplib.IMAP4_SSL('imap.gmail.com', 993)
     mail.login(config.EMAIL_ADDR, config.PASSWORD)
     mail.list()
-    mail.select("INBOX")
-    result, data = mail.uid('search', None, "ALL")
+    mail.select('INBOX')
+    result, data = mail.uid('search', None, 'ALL')
     i = len(data[0].split())
     new_payment = {}
     for x in range(i):
@@ -24,45 +24,45 @@ def get_messages(transaction_number):
         email_message = email.message_from_string(raw_email_string)
 
         email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
+        logger.info(
+            f"Verifing emails from {config.FROM_EMAIL} Check the sender of the mail if Payment cannot be processed")
 
         found = False
         for part in email_message.walk():
-            if part.get_content_type() == "text/plain":
+            if part.get_content_type() == 'text/plain':
                 body = part.get_payload()
                 buf = io.StringIO(body)
                 lines = buf.readlines()
                 count = 0
                 new_payment = {}
                 for line in lines:
-                    if "No. Transacci=C3=B3n" in line:
-                        if transaction_number in lines[count + 1].replace('\r', '').replace('\n', ''):
+                    if 'No. Transacci=C3=B3n' in line:
+                        if (transaction_number in lines[count + 1].replace('\r', '').replace('\n', '')) and \
+                                (config.FROM_EMAIL in email_from):
                             new_payment['transaction_number'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
                             found = True
-                            logger.info('Payment found on mail')
-                    if "Medio de Pago" in line and found:
-                        new_payment['payment_method'] = get_content_line(lines, count)
-                    if "Nombre" in line and found:
-                        new_payment['name'] = get_content_line(lines, count)
-                    if "Email" in line and found:
-                        new_payment['email'] = get_content_line(lines, count)
-                    if "Fecha y Hora" in line and found:
-                        new_payment['timestamp'] = get_content_line(lines, count)
-                    if "Tarjeta" in line and found:
-                        new_payment['card_number'] = get_content_line(lines, count)
-                    if "Producto Cantidad Precio Subtotal" in line and found:
-                        new_payment['order_detail'] = get_content_line(lines, count)
-                    if "Total del pago" in line and found:
+                    elif 'Medio de Pago' in line:
+                        new_payment['payment_method'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Nombre' in line:
+                        new_payment['name'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Email' in line:
+                        new_payment['email'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Fecha y Hora' in line:
+                        new_payment['timestamp'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Tarjeta' in line:
+                        new_payment['card_number'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Producto Cantidad Precio Subtotal' in line and found:
+                        new_payment['order_detail'] = lines[count + 1][2:].replace('\r', '').replace('\n', '')
+                    elif 'Total del pago' in line:
                         new_payment['total'] = lines[count][2:].replace('\r', '').replace('\n', '')
                     count += 1
+                if found:
+                    mail.close()
+                    mail.logout()
+                    return new_payment
             else:
                 continue
-        print(new_payment)
-        if len(new_payment) > 0 or x == i:
-            logger.info('All [INBOX] mail Read')
+        if x == i:
             mail.close()
             mail.logout()
             return new_payment
-
-
-def get_content_line(lines, count):
-    return lines[count + 1].replace('\r', '').replace('\n', '').replace('>', '')
