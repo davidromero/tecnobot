0c6af9227db8caf96f5366e3bb4834ba0e8a1c3d
davidromero <d-romero@live.com>
2020-08-13 23:21:13 -0600

    Delete middleware service, not longer necesary

--- middleware/chalicelib/config.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from chalice import CORSConfig
-
-TABLE_NAME = environ.get('TABLE_NAME')
-AWS_DEFAULT_REGION = environ.get('AWS_DEFAULT_REGION')
-
-cors_config = CORSConfig(
-    allow_origin='*'
-)
