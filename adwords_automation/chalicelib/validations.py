0cde184ec4df53909a0f6cf82e654630d95673ad
davidromero <d-romero@live.com>
2020-09-10 02:00:36 -0600

    TECNOBOT-73 final validations added

--- adwords_automation/chalicelib/validations.py
+++ adwords_automation/chalicelib/validations.py
@@ -4,17 +4,27 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
 
-available_locations = ['Mexico', 'Guatemala', 'El Salvador', 'Honduras', 'Nicaragua', 'Costa Rica', 'Panamá',
-                       'Colombia']
+available_locations = ['mexico', 'guatemala', 'el salvador', 'honduras', 'nicaragua', 'costa rica', 'panamá',
+                       'colombia']
 available_packages = ['MARKETING_COMBO_1', 'MARKETING_COMBO_2', 'MARKETING_COMBO_3']
 body_fields = ['business_name', 'description', 'history', 'location', 'marketing_package', 'phone', 'psid',
                'search_terms', 'slogan', 'website']
 
 
-def validate_business_name_descrip_slogan(business_name, description, slogan):
-    if validate_word_medsize(business_name) and validate_word_medsize(description) and validate_word_medsize(slogan):
-        return True
-    return False
+def validate_body(body):
+    if validate_fields(body):
+        valid = True
+        valid = valid and validate_word_medsize(body['business_name'])
+        valid = valid and validate_word_medsize(body['description'])
+        valid = valid and validate_word_medsize(body['slogan'])
+        valid = valid and validate_phone_number(body['phone'])
+        valid = valid and validate_history(body['history'])
+        valid = valid and validate_website(body['website'])
+        valid = valid and validate_location(body['location'])
+        valid = valid and validate_marketing_package(body['marketing_package'])
+        return valid
+    else:
+        return False
 
 
 def validate_word_medsize(word):
@@ -33,17 +43,6 @@ def validate_phone_number(number):
     return False
 
 
-def validate_body(body):
-    if validate_fields(body):
-        valid = True
-        valid = valid and validate_website(body['website'])
-        valid = valid and validate_location(body['location'])
-        valid = valid and validate_marketing_package(body['marketing_package'])
-        return valid
-    else:
-        return False
-
-
 def validate_fields(body):
     for body_key in body_fields:
         if body_key not in body.keys():
@@ -57,19 +56,21 @@ def validate_fields(body):
 
 
 def validate_website(website):
-    if re.match(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}'
-                r'(:[0-9]{1,5})?(\/.*)?$', website, re.IGNORECASE):
-        return True
-    return False
+    return re.match(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}'
+                    r'(:[0-9]{1,5})?(\/.*)?$', website, re.IGNORECASE)
 
 
 def validate_location(location):
-    if location in available_locations:
-        return True
-    return False
+    return location.lower() in available_locations
 
 
 def validate_marketing_package(marketing_package):
-    if marketing_package in available_packages:
-        return True
-    return False
+    return marketing_package.upper() in available_packages
+
+
+def validate_psid(psid):
+    return len(psid) > 1 and psid.isdigit()
+
+
+def validate_search_terms(search_terms):
+    return search_terms is not None and len(search_terms) > 1
